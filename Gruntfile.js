94fcfc060aad0538b1de2fe73e1893461f0541af
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-02-04 15:14:55 +0000

    Changed grunt file and removed jasmine

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,16 +9,10 @@ module.exports = function(grunt){
 				src: ['test/**/*']
 			}
 		},
-		jasmine_node: {
-			options: {
-				forceExit: true,
-			},
-			all: ['spec/']
-		},
 	});
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-mocha-casperjs');
-	grunt.loadNpmTasks('grunt-jasmine-node');
 
 	grunt.registerTask('default', ['mocha_casperjs']);
-	grunt.registerTask('jasmine', ['jasmine_node']);
+
 };
