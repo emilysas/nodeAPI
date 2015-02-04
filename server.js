a98d00a5e7ab2999a671ac6428c03186eb1e47b5
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-02-04 17:59:35 +0000

    Implemented ability to send name

--- server.js
+++ server.js
@@ -9,17 +9,11 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(request, response){
-	response.render('index', { name: "Jake & Emily"} );
-});
-
-app.post('/name', function(req, res){
-  var name = req.body.name
-  console.log(name);
-  res.render('index', { name: name } );
+	response.render('index', request.query);
 });
 
 app.get('/json_name', function(req, res){
-  res.json(req.query);
+  res.json('reflect', {query: req.query});
 });
 
 server.listen(3000, function(){
--- test/homepage_features.js
+++ test/homepage_features.js
@@ -4,31 +4,6 @@ describe('homepage', function(){
     casper.start('http://localhost:3000/');
   });
 
-	// it('greeting', function(){
-	// 	casper.waitForSelector('form', function() {
-
-	// 		this.fill("form#nameform", {
-	// 			'name' : 'Emily',
-	// 		}, true);
-
-	// 	});
- //    casper.then(function() {
- //      expect('h1').to.have.text('HI Emily');
- //    });
-	// });
-
-  // it('tweet', function() {
-  //   casper.waitForSelector('#tweetform', function() {
-
-  //       this.fill("#tweetform#find-twitter-account", {
-  //         'account' : '@sasemily',
-  //       }, true);
-
-  //   });
-  //   casper.then(function() {
-  //     expect('#tweet').to.have.text("Hey @Jakobtek your code smells.");
-  //   });
-  // });
 	it('json loveliness', function() {
 		casper.waitForSelector('form', function() {
 
@@ -39,7 +14,21 @@ describe('homepage', function(){
 		});
 		casper.then(function() {
       expect('body').to.have.text("{\"name\":\"Emily\"}");
-		})
-	})
+		});
+	});
+
+  it('json hello', function() {
+    casper.waitForSelector('form', function() {
+
+      this.fill("form#nameform", {
+        'name' : 'Emily',
+      }, true);
+
+    });
+    casper.then(function() {
+      expect('body').to.have.text("Hi Emily");
+    });
+  });
+
 
 });
