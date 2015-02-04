5eacbb957be80cc23cbdd70bdc2c5ced6a09775a
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-04 12:35:38 +0000

    creating test for user-login

--- test/homepage_features.js
+++ test/homepage_features.js
@@ -1,12 +1,29 @@
+
+
+
 describe('homepage', function(){
   before(function(){
     casper.start('http://localhost:3000/');
   });
 
-  it('hello worlds', function(){
-    casper.then(function(){
-      expect("body").to.have.text("Hello world");
-    });
-  });
+  // it('hello worlds', function(){
+  //   casper.then(function(){
+  //     expect("body").to.have.text("Hello world");
+  //   });
+  // });
+
+	it('greeting', function(){
+		casper.waitForSelector("form input[name='name']", function(){
+
+			this.fillSelectors('form#user-login', {
+				'input[name = name]' : "Emily",
+			});
+
+		}, true);
+		
+		// casper.then(function(){
+			expect("body").to.have.text("HELLO CREATOR");
+		// });
+	});
 
 });
