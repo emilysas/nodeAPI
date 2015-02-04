f67636178a2f71baaa9bab5bdf2a155eab592df2
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-02-04 15:17:07 +0000

    Enabled the ability to receive post requests and parse them.

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var server = require('http').createServer(app);
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(request, response){
-	response.render('index', { names: "Jake & Emily"} );
+	response.render('index', { name: "Jake & Emily"} );
+});
+
+app.post('/name', function(req, res){
+  var name = req.body.name
+  console.log(name);
+  res.render('index', { name: name } );
 });
 
 server.listen(3000, function(){
