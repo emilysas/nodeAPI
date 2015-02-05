3cddc0aaf19070aa38a96e092a3fbd8ab5333f8c
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-05 15:53:24 +0000

    starting Github API Simulation - first test passes

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 var server = require('http').createServer(app);
-var jacob = require('./app/jacob')
+// var bodyParser = require('body-parser');
+// var jacob = require('./app/jacob')
 
-app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/views'));
+// app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(request, response){
-	response.render('index', request.query);
+	response.send('Github API Simulation');
 });
 
-app.get('/json_name', function(req, res){
-  res.json('json_name', {query: req.query});
-});
+// app.get('/json_name', function(req, res){
+//   res.json('json_name', {query: req.query});
+// });
 
-app.get('/jacob', function(req, res){
-	console.log(jacob());
-	res.json('jacob', {query: jacob.query});
-})
+// app.get('/jacob', function(req, res){
+// 	console.log(jacob());
+// 	res.json('jacob', {query: jacob.query});
+// })
 
 server.listen(3000, function(){
 	console.log("Server listening on port 3000");
--- test/homepage_features.js
+++ test/homepage_features.js
@@ -1,34 +1,48 @@
-describe('homepage', function(){
+// var chai = require('chai');
+// var expect = chai.expect;
+
+var host = 'http://localhost:3000';
+
+describe('Github API Simulation', function(){
 
   before(function(){
-    casper.start('http://localhost:3000/');
+    casper.start(host);
   });
 
-	it('json loveliness', function() {
-		casper.waitForSelector('form', function() {
+	it('should be a Github API Simulation', function(){
+  	casper.then(function(){
+			expect('body').to.have.text('Github API Simulation')
+		});	
+  })
 
-			this.fill("form#nameform", {
-				'name' : 'Emily',
-			}, true);
-
-		});
-		casper.then(function() {
-      expect('body').to.have.text("{\"name\":\"Emily\"}");
-		});
-	});
-
-  it('json hello', function() {
-    casper.waitForSelector('form', function() {
-
-      this.fill("form#nameform", {
-        'name' : 'Emily',
-      }, true);
-
-    });
-    casper.then(function() {
-      expect('body').to.have.text("Hi Emily");
-    });
-  });
 
 
 });
+	// it('json loveliness', function() {
+	// 	casper.waitForSelector('form', function() {
+
+	// 		this.fill("form#nameform", {
+	// 			'name' : 'Emily',
+	// 		}, true);
+
+	// 	});
+	// 	casper.then(function() {
+ //      expect('body').to.have.text("{\"name\":\"Emily\"}");
+	// 	});
+	// });
+
+ //  it('json hello', function() {
+ //    casper.waitForSelector('form', function() {
+
+ //      this.fill("form#nameform", {
+ //        'name' : 'Emily',
+ //      }, true);
+
+ //    });
+ //    casper.then(function() {
+ //      expect('body').to.have.text("Hi Emily");
+ //    });
+ //  });
+
+
+
