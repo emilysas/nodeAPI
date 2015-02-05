e23623698fa03212130462959bbb9c96d7b2d00b
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-05 16:13:45 +0000

    writing test - should show github json

--- test/homepage_features.js
+++ test/homepage_features.js
@@ -15,6 +15,14 @@ describe('Github API Simulation', function(){
 		});	
   })
 
+  it('should show a github JSON for a particular user', function(){
+  	casper.thenOpen(host + '/users/emilysas', function(){
+  		expect(response.headers.get(Access-Control-Allow-Origin).to.equal('*'));
+  		expect(response.headers.get('Content-Type').to.equal('application/json; charset=utf-8'));
+  		expect('body').to.have.text('{"login": "emilysas", "id": 7489940,, "avatar_url": "https://avatars.githubusercontent.com/u/7489940?v=3", "followers": 14,"following": 12}');
+  	})
+  })
+
 
 
 });
